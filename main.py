970115dfac3ac0627e58dd1922b08d24a249304a
Kellan Butler <87137401+kellantech@users.noreply.github.com>
2023-02-18 12:59:47 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO,emit
+import json,cppimport
+import scramble
+
+
+clib = cppimport.imp("clib")
+
+
+app = Flask(__name__)
+sapp = SocketIO(app)
+
+@app.route("/")
+def index():
+	return render_template("index.html")
+
+@sapp.on('getIDR')
+def connect(d):
+	r = d["data"]
+	print(f"USED ID {r}")
+	
+@sapp.on('setEVN')
+def sevn(d):
+	i = int(d["id"])
+	evn = d["evn"]
+	clib.set_event(i,evn)
+
+@sapp.on("update")
+def handle_ge(data):
+	d = json.loads(data['data'])
+	d['id'] = str(d['id'])
+	clib.parse(d)
+	
+	
+	emit("retupdate",[json.dumps(clib.get(int(d["id"]))),int(d["id"])], broadcast=True)
+
+@sapp.on("newr")
+def handle_nr(data):
+	d = int(data['data'])
+	clib.clear(d)
+	
+	emit("retupdate",["{}",d], broadcast=True)
+	event  = clib.get_event(d)
+	scr = scramble.scrambled[event]()
+	emit("newscramb",[scr,d],broadcast=True)
+
+
+
+
+
+	
+sapp.run(app,host="0.0.0.0")
