48e33001db901de2c5e684ff356517d255b14962
Kellan Butler <87137401+kellantech@users.noreply.github.com>
2023-05-22 15:57:05 -0400

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,23 +1,22 @@
-        document.getElementById("x0").addEventListener("click", () => {
-            document.getElementById("b0").style.display = "none"
+document.getElementById("x0").addEventListener("click", () => {
+	document.getElementById("b0").style.display = "none"
 
-        })
-        
-        document.getElementById("o1").addEventListener("click", () => {
-            document.getElementById("b0").style.display = "block"
+})
+
+document.getElementById("o1").addEventListener("click", () => {
+	document.getElementById("b0").style.display = "block"
 
 
-        })
-        
-        
-        
+})
+
+
+
 document.getElementById("x1").addEventListener("click", () => {
-            document.getElementById("b1").style.display = "none"
+	document.getElementById("b1").style.display = "none"
 
-        })
-        
-        document.getElementById("o2").addEventListener("click", () => {
-            document.getElementById("b1").style.display = "block"
+})
 
-        })
+document.getElementById("o2").addEventListener("click", () => {
+	document.getElementById("b1").style.display = "block"
 
+})
